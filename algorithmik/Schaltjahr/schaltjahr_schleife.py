92537804459c0db44c271a64fa3c44b94cc6c5e5
Stefan Spieker <S.Spieker@gmx.net>
2020-01-28 22:17:38 +0100

    schleife hinzugefügt und schaltjahr_schleife fertig

--- algorithmik/Schaltjahr/schaltjahr_schleife.py
+++ algorithmik/Schaltjahr/schaltjahr_schleife.py
@@ -7,31 +7,36 @@ def jahreszahl_einlesen():
         exit(1)
     return jahr
 
+
+def ist_schaltjahr(jahreszahl):
+    schaltjahr = False
+    if jahreszahl % 4 == 0:
+        if jahreszahl % 100 == 0:
+            if jahreszahl % 400 == 0:
+                schaltjahr = False
+            else:
+                # nicht durch 400 teilbar.
+                schaltjahr = True
+        else:
+            # nicht durch 100 teilbar.
+            schaltjahr = True
+    else:
+        # nicht durch 4 teilbar.
+        schaltjahr = False
+    return schaltjahr
+
+
 print("Bitte Start-Jahreszahl eingeben:")
 start = jahreszahl_einlesen()
 print("Bitte End-Jahreszahl eingeben:")
 ende = jahreszahl_einlesen()
 
-print("Es werden", ende-start, "Jahre berechnet")
+print("Es werden", ende - start, "Jahre berechnet")
 # Dein Programmcode
+jahr = start
+print("Folgende Jahre sind Schaltjahre:")
+while jahr <= ende:
+    if (ist_schaltjahr(jahr)):
+        print(jahr, end=', ')
+    jahr = jahr + 1
 
-
-schaltjahr = False
-if jahr % 4 == 0:
-    if jahr % 100 == 0:
-        if jahr % 400 == 0:
-            schaltjahr = False
-        else:
-            # nicht durch 400 teilbar.
-            schaltjahr = True
-    else:
-        # nicht durch 100 teilbar.
-        schaltjahr = True
-else:
-    # nicht durch 4 teilbar.
-    schaltjahr = False
-
-if schaltjahr:
-    print("Es ist ein Schaltjahr!")
-else:
-    print("Kein Schaltjahr!")
---
+++ algorithmik/schleife.py
@@ -0,0 +1,6 @@
+laufvariable = 0
+ende = 1000
+while laufvariable <= ende:
+    if laufvariable % 13 == 0:
+        print(laufvariable, "ist durch 13 teilbar")
+    laufvariable = laufvariable + 1
