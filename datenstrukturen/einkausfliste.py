d03de23de9bfd601a73edcab9dad2e9c0ad48284
Stefan Spieker <S.Spieker@gmx.net>
2020-09-15 21:52:44 +0200

    einkaufsliste überarbeitet

--- datenstrukturen/einkausfliste.py
+++ datenstrukturen/einkausfliste.py
@@ -5,11 +5,15 @@ print("Ich habe", len(einkaufsliste), "Dinge einzukaufen.")
 
 print("Diese Dinge sind: ", end="")
 for ding in einkaufsliste:
-    print(ding, end=", ")
+    print(ding, end=", ")  # das end=", " führt zu einem Komma nach jedem Element ohne Zeilenumbruch
 
-print()
-einkaufsliste.append("Reis")
-einkaufsliste.sort()
+print()  # neue Zeile ausgeben
+
+einkaufsliste.append("Reis") 
+
+einkaufsliste.sort() # sortiert die einkausliste alphabetisch
+
+print("Ich habe", len(einkaufsliste), "Dinge einzukaufen.")
 
 print("Diese Dinge sind: ", end="")
 for ding in einkaufsliste:
